2fd7584fd5c426c43119ee97c7b1d3a1a5bc3d58
qianqians <theDarkforce@aliyun.com>
2020-01-03 21:17:16 +0800

    enet_host_create

--- js_enet.cpp
+++ js_enet.cpp
@@ -10,7 +10,7 @@
 #endif
 
 void throw_v8_exception(v8::Isolate *isolate, std::string msg) {
-    isolate->ThrowException(v8::Exception::TypeError(v8::String::NewFromUtf8(isolate, msg.c_str())));
+    isolate->ThrowException(v8::Exception::TypeError(v8::String::NewFromUtf8(isolate, msg.c_str(), v8::NewStringType::kNormal).ToLocalChecked()));
 }
 
 void js_enet_initialize(const v8::FunctionCallbackInfo <v8::Value> &args) {
@@ -25,9 +25,39 @@ void js_enet_deinitialize(const v8::FunctionCallbackInfo <v8::Value> &args) {
     enet_deinitialize();
 }
 
+void js_enet_host_create(const v8::FunctionCallbackInfo <v8::Value> &args) {
+    auto isolate = args.GetIsolate();
+
+    if (!args[0]->IsString() || !args[1]->IsUint32() || !args[2]->IsUint32()) {
+        throw_v8_exception(isolate, "Wrong arguments");
+        return;
+    }
+
+    v8::Local<v8::Context> context = v8::Context::New(isolate);
+    v8::String::Utf8Value ip(isolate, args[0]);//->ToString(context).ToLocalChecked());
+    uint16_t port = args[1]->Uint32Value(context).FromJust();
+    uint32_t peercount = args[2]->Uint32Value(context).FromJust();
+
+    ENetAddress address;
+    if (enet_address_set_host_ip(&address, *ip) != 0){
+        throw_v8_exception(isolate, "enet_address_set_host_ip faild");
+        return;
+    }
+    address.port = port;
+
+    ENetHost* host = enet_host_create(&address, peercount, 1, 0, 0);
+    if (host == nullptr){
+        throw_v8_exception(isolate, "enet_host_create faild");
+        return;
+    }
+
+    args.GetReturnValue().Set(1);
+}
+
 void init(v8::Local <v8::Object> exports) {
     NODE_SET_METHOD(exports, "enet_initialize", js_enet_initialize);
     NODE_SET_METHOD(exports, "enet_deinitialize", js_enet_deinitialize);
+    NODE_SET_METHOD(exports, "enet_host_create", js_enet_host_create);
 }
 
 NODE_MODULE(js_enet, init);
