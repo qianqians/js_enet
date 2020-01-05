4e3f3ed117d99915046df2ba7f2d4c2fb52aabcf
吴铭 <451517996@qq.com>
2020-01-05 23:17:00 +0800

    Signed-off-by: 吴铭 <451517996@qq.com>

--- js_enet.cpp
+++ js_enet.cpp
@@ -4,6 +4,7 @@
 
 #include <map>
 #include <string>
+#include <iostream>
 
 #include "enet/enet.h"
 
@@ -99,32 +100,40 @@ void js_enet_host_service(const v8::FunctionCallbackInfo<v8::Value> &args) {
     uint32_t _h = args[0]->Uint32Value(context).FromJust();
     ENetHost* host = ENetHostSet[_h];
 
-    v8::Local<v8::Object> obj = v8::Object::New(isolate);
-
     ENetEvent event;
-    if (enet_host_service (host, & event, 0) > 0)
+    if (enet_host_service(host, &event, 10) > 0)
     {
-        v8::Local<v8::Integer> type = v8::Uint32::NewFromUnsigned(isolate, (uint32_t)ENET_EVENT_TYPE_CONNECT);
+        v8::Local<v8::Object> obj = v8::Object::New(isolate);
+
+        v8::Local<v8::Integer> type = v8::Uint32::NewFromUnsigned(isolate, (uint32_t)event.type);
         obj->Set(v8::String::NewFromUtf8(isolate, "type", v8::NewStringType::kNormal).ToLocalChecked(), type);
 
         switch (event.type)
         {
         case ENET_EVENT_TYPE_CONNECT:
             {
+                char ip[256];
+                enet_address_get_host_ip(&event.peer->address, ip, 256);
+                v8::Local<v8::String> _ip = v8::String::NewFromUtf8(isolate, ip, v8::NewStringType::kNormal).ToLocalChecked();
                 v8::Local<v8::Integer> host = v8::Uint32::NewFromUnsigned(isolate, (uint32_t)event.peer->address.host);
                 v8::Local<v8::Integer> port = v8::Uint32::NewFromUnsigned(isolate, (uint32_t)event.peer->address.port);
                 obj->Set(v8::String::NewFromUtf8(isolate, "host", v8::NewStringType::kNormal).ToLocalChecked(), host);
+                obj->Set(v8::String::NewFromUtf8(isolate, "ip", v8::NewStringType::kNormal).ToLocalChecked(), _ip);
                 obj->Set(v8::String::NewFromUtf8(isolate, "port", v8::NewStringType::kNormal).ToLocalChecked(), port);
 
-                uint64_t peerHandle = (uint64_t)event.peer->address.host << 32 || event.peer->address.port;
+                uint64_t peerHandle = (uint64_t)event.peer->address.host << 32 | event.peer->address.port;
                 ENetPeerSet.insert(std::make_pair(peerHandle, event.peer));
             }
             break;
         case ENET_EVENT_TYPE_RECEIVE:
             {
+                char ip[256];
+                enet_address_get_host_ip(&event.peer->address, ip, 256);
+                v8::Local<v8::String> _ip = v8::String::NewFromUtf8(isolate, ip, v8::NewStringType::kNormal).ToLocalChecked();
                 v8::Local<v8::Integer> host = v8::Uint32::NewFromUnsigned(isolate, (uint32_t)event.peer->address.host);
                 v8::Local<v8::Integer> port = v8::Uint32::NewFromUnsigned(isolate, (uint32_t)event.peer->address.port);
                 obj->Set(v8::String::NewFromUtf8(isolate, "host", v8::NewStringType::kNormal).ToLocalChecked(), host);
+                obj->Set(v8::String::NewFromUtf8(isolate, "ip", v8::NewStringType::kNormal).ToLocalChecked(), _ip);
                 obj->Set(v8::String::NewFromUtf8(isolate, "port", v8::NewStringType::kNormal).ToLocalChecked(), port);
 
                 v8::MaybeLocal<v8::Object> data = node::Buffer::Copy(isolate, (char*)event.packet -> data, event.packet -> dataLength);
@@ -136,19 +145,25 @@ void js_enet_host_service(const v8::FunctionCallbackInfo<v8::Value> &args) {
         
         case ENET_EVENT_TYPE_DISCONNECT:
             {
+                char ip[256];
+                enet_address_get_host_ip(&event.peer->address, ip, 256);
+                v8::Local<v8::String> _ip = v8::String::NewFromUtf8(isolate, ip, v8::NewStringType::kNormal).ToLocalChecked();
                 v8::Local<v8::Integer> host = v8::Uint32::NewFromUnsigned(isolate, (uint32_t)event.peer->address.host);
                 v8::Local<v8::Integer> port = v8::Uint32::NewFromUnsigned(isolate, (uint32_t)event.peer->address.port);
                 obj->Set(v8::String::NewFromUtf8(isolate, "host", v8::NewStringType::kNormal).ToLocalChecked(), host);
+                obj->Set(v8::String::NewFromUtf8(isolate, "ip", v8::NewStringType::kNormal).ToLocalChecked(), _ip);
                 obj->Set(v8::String::NewFromUtf8(isolate, "port", v8::NewStringType::kNormal).ToLocalChecked(), port);
 
-                uint64_t peerHandle = (uint64_t)event.peer->address.host << 32 || event.peer->address.port;
+                uint64_t peerHandle = (uint64_t)event.peer->address.host << 32 | event.peer->address.port;
                 ENetPeerSet.erase(peerHandle);
             }
             break;
         }
+        args.GetReturnValue().Set(obj);
+        return;
     }
 
-    args.GetReturnValue().Set(obj);
+    args.GetReturnValue().SetNull();
 }
 
 void js_enet_peer_send(const v8::FunctionCallbackInfo<v8::Value> &args) {
@@ -166,7 +181,7 @@ void js_enet_peer_send(const v8::FunctionCallbackInfo<v8::Value> &args) {
 
     ENetHost* host = ENetHostSet[_h];
 
-    uint64_t peerHandle = (uint64_t)_host << 32 || _port;
+    uint64_t peerHandle = (uint64_t)_host << 32 | _port;
     ENetPeer* peer = ENetPeerSet[peerHandle];
 
     char* data = node::Buffer::Data(args[3]);
@@ -180,23 +195,29 @@ void js_enet_peer_send(const v8::FunctionCallbackInfo<v8::Value> &args) {
 void js_enet_host_connect(const v8::FunctionCallbackInfo<v8::Value> &args){
     auto isolate = args.GetIsolate();
 
-    if (!args[0]->IsUint32() || !args[1]->IsUint32() || !args[2]->IsUint32()) {
+    if (!args[0]->IsUint32() || !args[1]->IsString() || !args[2]->IsUint32()) {
         throw_v8_exception(isolate, "Wrong arguments");
         return;
     }
 
     v8::Local<v8::Context> context = v8::Context::New(isolate);
     uint32_t _h = args[0]->Uint32Value(context).FromJust();
-    uint32_t _host = args[1]->Uint32Value(context).FromJust();
+    v8::String::Utf8Value _ip(isolate, args[1]);
     uint16_t _port = args[2]->Uint32Value(context).FromJust();
 
     ENetHost* host = ENetHostSet[_h];
 
     ENetAddress address;
-    address.host = _host;
+    if (enet_address_set_host_ip(&address, *_ip) != 0){
+        throw_v8_exception(isolate, "enet_address_set_host_ip faild");
+        return;
+    }
     address.port = _port;
 
-    enet_host_connect(host, &address, 1, 0);   
+    ENetPeer* peer = enet_host_connect(host, &address, 1, 0);
+    if (peer == nullptr){
+        throw_v8_exception(isolate, "enet_host_connect faild");
+    }
 }
 
 
